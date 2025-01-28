ec707a7371be45936194b7f734e953b531c07a92
kumarvivek13025 <106479450+kumarvivek13025@users.noreply.github.com>
2025-01-28 22:40:13 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div>Hello from Vivek Kumar</div>;
+
+export default App;
---
+++ client/src/config.js
@@ -0,0 +1,1 @@
+export const API = 'http://localhost:5000/api' || 'https://ecommerce-ak.herokuapp.com/api';
